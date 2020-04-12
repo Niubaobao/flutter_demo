9fbe84704b3f3a2c884867382691c9ae7900832f
张亚超 <zhangyachao@xiaoheiban.cn>
2020-04-12 23:16:22 +0800

    feat#增加flow/flex布局

--- lib/demo/search.dart
+++ lib/demo/search.dart
@@ -32,9 +32,30 @@ class _SearchPageState extends State<SearchPage> {
     }
   }
 
+  List<int> hotGoodsList = [1, 2, 3, 4, 5, 6, 7];
+  listWidget(double width) {
+    List<Widget> listWidget = hotGoodsList.map((val) {
+      return Container(
+          width: (width / 2),
+          height: 100,
+          color: Colors.red,
+          padding: EdgeInsets.all(5.0),
+          margin: EdgeInsets.only(bottom: 3.0),
+          child: Text(val.toString()));
+    }).toList();
+    return Wrap(
+      runSpacing: 4.0, // 纵轴（垂直）方向间距
+      alignment: WrapAlignment.start, //沿主轴方向居中
+      // spacing: 2,
+      children: listWidget,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     const String toLaunch = 'https://www.baidu.com/';
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
 
     return Scaffold(
       appBar: AppBar(
@@ -54,7 +75,8 @@ class _SearchPageState extends State<SearchPage> {
                 _launched = _makePhoneCall('tel:$_phone');
               }),
               child: Text('拨打电话'),
-            )
+            ),
+            listWidget(width)
           ],
         ),
       ),
