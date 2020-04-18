2ebf315e2b0d8d3defe0ba63b9e00170399263cb
张亚超 <zhangyachao@xiaoheiban.cn>
2020-04-18 20:34:34 +0800

    feat#使用mixin

--- lib/demo/lesson/Lesson1.dart
+++ lib/demo/lesson/Lesson1.dart
@@ -1,3 +1,4 @@
+import 'package:demo/data/lesson.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -6,11 +7,31 @@ class Lesson1 extends StatefulWidget {
   _Lesson1State createState() => _Lesson1State();
 }
 
-class _Lesson1State extends State<Lesson1> {
+class _Lesson1State extends State<Lesson1> with LessonText {
   @override
   void initState() {
     print('initState');
+    initLesson1();
     super.initState();
+  }
+
+  initLesson1() {
+    //如果函数只有一条语句， 可以使用箭头简写,如函数体只有一句话，不需要有大括号
+    var list = ['lesson1', 'lesson2', 'lesson3'];
+    list.forEach(
+      (item) => print('${list.indexOf(item)}:$item'),
+    );
+    //算术运算符
+    print('5/2=${5 / 2}结果是双浮点型');
+    print('5~/2=${5 ~/ 2}结果是整型');
+    print('5%2=${5 % 2}取余');
+    var a, b;
+    a = 0;
+    b = ++a; //a先自增在赋值给b
+    b = a++; //a先赋值给b，在进行自增
+    print(b);
+    //调用mixin方法
+    entertainMe();
   }
 
   @override
